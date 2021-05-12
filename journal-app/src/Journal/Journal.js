cecdd273038db25acd2c668d5aa7656ad081676e
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-05-11 19:11:15 -0700

    componentDidMount

--- journal-app/src/Journal/Journal.js
+++ journal-app/src/Journal/Journal.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import firebase from 'firebase';
+
+const db = firebase.firestore();
 
 export default class Journal extends React.Component {
+    componentDidMount() {
+        //not in real time
+        //     db.collection('journalEntries')
+        //         .get()
+        //         .then((querySnapshot) => {
+        //             querySnapshot.forEach((doc) => {
+        //                 console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        //             });
+        //         });
+
+
+        db.collection('journalEntries')
+            .onSnapshot((doc) => {
+                console.log(doc)
+            })
+    }
+
     render() {
         return (
             <div>
