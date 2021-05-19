3b1215389320121abc665b291075058fba8f292e
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-05-18 18:49:07 -0700

    successfully signed in with google auth

--- journal-app/src/Journal/Home.js
+++ journal-app/src/Journal/Home.js
@@ -9,9 +9,7 @@ const uiConfig = {
     signInSuccessUrl: '/signedIn',
     // We will display Google and Facebook as auth providers.
     signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,    
     ],
   };
 
